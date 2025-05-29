1782c72f47f182a648af9f86e01a76dcd0ab2848
Manail <manahilghouri100@gmail.com>
2025-05-29 16:04:47 +0500

    Initial commit - Expense Tracker Streamlit App

---
+++ data_manager.py
@@ -0,0 +1,1 @@
+ # Load, save, update, and delete expenses
---
+++ main.py
@@ -0,0 +1,5 @@
+  # Main file to run the app
+import streamlit as st
+
+st.title("Expense Tracker")
+st.write("Upload a receipt or add an expense manually.")
